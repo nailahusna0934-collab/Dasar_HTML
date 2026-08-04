2a22070a6ce514012b1768e2b16d017abe27602b
nailahusna0934-collab <nailahusna0934@gmail.com>
2026-08-04 08:25:37 +0700

    Initial commit

---
+++ html_fundamental.php
@@ -0,0 +1,107 @@
+<html>
+    <head>
+        <title>
+            Dasar Dasar HTML
+        </title>
+    </head>
+    <body>  
+        <h1 align="center">
+             Pengenalan Atribut HTML
+        </h1>
+    </body>
+</html>
+        <h2 align="center">
+             Pengenalan Atribut HTML
+        </h2>
+        <h3 align="center">
+             Pengenalan Atribut HTML
+        </h3>
+        <h4 align="center">
+             Pengenalan Atribut HTML
+        </h4>
+        <h5 align="center">
+             Pengenalan Atribut HTML
+        </h5>
+        <h6 align="center">
+             Pengenalan Atribut HTML
+        </h6>
+    <body>
+        <b>ini format format text tebal</b><br/>
+        <i>ini format text italic</i><br/>
+        <u>ini format text underline</u><br/>
+        <em>ini format text em</em><br/>
+        <small>ini format text small</small><br/>
+        <strong>ini format text strong</strong><br/>
+        <sub>ini format text sub</sub><br/>
+        <sup>ini format text sup</sup><br/>
+        <ins>ini format text ins</ins><br/>
+        <del>ini format text del</del><br/>
+        <mark>ini format text mark</mark>
+    </body>
+
+    <body>
+        <p align="left">1. ini merupakan penulisan paragraf rata kiri
+            dalam dokument HTML</p>
+        <p align="right">2. ini merupakan penulisan paragraf rata
+            kanan dalam dokument HTML</p>
+        <p align="center">3.ini merupakan penulisan paragraf rata
+            tengah dalam dokument HTML</p>
+        <p align="justify">4. ini merupakan penulisan paragraf rata
+            kiri kanan dalam dokument HTML</p>
+    </body>
+
+    <body>
+        <table border="1">
+        <tr>
+            <td>baris1 / kolom 1</td>
+            <td>baris 1 / kolom 2</td>
+            <td>baris 1 / kolom 3</td>
+        </tr>
+        <tr>
+            <td>baris 2 / kolom 1</td>
+            <td>baris 2 / kolom 2</td>
+            <td>baris 2 / kolom 3</td>
+        </tr>
+        <tr>
+            <td>baris 3/ kolom 1</td>
+            <td>baris 3/ kolom 2</td>
+            <td>baris3/ kolom 3</td>
+        </tr>
+        <tr>
+            <td>baris 4/ kolom 1</td>
+            <td>baris 4/ kolom 2</td>
+            <td>baris 4/ kolom 3</td>
+        </tr>
+        </table>
+
+    </body>
+        <ul>
+            <li>belajar HTML</li>
+            <li>belajar PHP</li>
+        </ul>
+    </body>
+    <body>
+        <form>
+            nama : <input type="text" name="nama"><br/>
+            alamat : <input type="text" name="alamat"><br/>
+            password : <input type="password" name="pass"><br/>
+            Jenis Kelamin :
+                <input type="radio" name="jk" value="laki-laki">Laki-Laki
+                <input type="radio" name="jk" value="perempuan">Perempuan
+                <br/> 
+                bahas pemrograman yang dikuasai :
+                <input type="checkbox" name="web">Web 
+                <input type="checkbox" name="mobile">MOBILE
+                <input type="checkbox" name="desk">DESKTOP 
+            kota asal :
+                <select>
+                    <option>Semarang</option>
+                    <option>Bandung</option>
+                </select>
+            <br/> 
+            pesan anda :
+            <textarea></textarea>
+            <br/>
+            <input type="submit" value="proses">
+        </form>
+    </body>
